069729829e53a22cdf5d8627701bf9e8c3734cbb
Ravi Dhyani <ravi.dhyani@ravidhy-ltm3zpb.internal.salesforce.com>
2022-05-18 16:59:11 +0100

    update

--- deployApp.sh
+++ deployApp.sh
@@ -2,8 +2,30 @@
 
 deployApp () {
   echo "Deploy App"
-  return 33
+  return $1
+}
+
+
+function getAppsList(){
+    local offset=0
+    local limit=100
+    local apps_in_page=1
+    local appsJson=""
+
+    if [[ -z $APPS_LIST ]]; then 
+        APPS_LIST="[]"
+    fi 
+
+    while [[ ${apps_in_page} -gt 0 ]]; do 
+        appsJson="$(anypoint-cli runtime-mgr cloudhub-application list --limit=${limit} --offset=${offset} -o json | jq .)"
+        apps_in_page=$(echo "$appsJson" | jq . | jq length)
+        if [[ ${apps_in_page} -gt 0 ]]; then 
+            APPS_LIST=$(echo "$APPS_LIST" | jq ". += $appsJson")
+        fi
+        ((offset=$offset+$limit))
+    done
 }
 
 deployApp
-echo $?
+getAppsList
+echo $1
