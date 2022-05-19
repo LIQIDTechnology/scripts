4aad0c049a019eb144f188ac40e10a2f949049b9
Ravi Dhyani <ravi.dhyani@ravidhy-ltm3zpb.internal.salesforce.com>
2022-05-19 11:38:24 +0100

    update

--- deployApp.sh
+++ deployApp.sh
@@ -1,15 +1,16 @@
-
+#!/bin/bash
 
 checkStatus(){
 #export ANYPOINT_PROFILE="connAppProfile"
-anypoint-cli
+#source ./exports.sh
+echo "$ANYPOINT_PROFILE"
 local appName=$1
 echo "--App name->${appName}"
 local lowApp=$( echo "$appName" | tr '[:upper:]' '[:lower:]')
 echo "--Low App name->${lowApp}"
-
-
+anypoint-cli
 appStatus="$(anypoint-cli runtime-mgr cloudhub-application describe "$lowApp" -o json | jq .Status)"
+#appStatus="$(anypoint-cli runtime-mgr cloudhub-application describe content -o json | jq)"
 
 echo "----->App status ->$appStatus<-"
 
