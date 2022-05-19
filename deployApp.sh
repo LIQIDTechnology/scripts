0b3263e6f034ad41a71b92dbc770e1bf806479e3
Ravi Dhyani <ravi.dhyani@ravidhy-ltm3zpb.internal.salesforce.com>
2022-05-19 09:21:51 +0100

    update

--- deployApp.sh
+++ deployApp.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-deployApp () {
-  echo "Start Deploy App"
-  return $1
-}
 
-testApp(){
+checkStatus(){
 export ANYPOINT_PROFILE="connAppProfile"
+
+local appName=$1
+
+appStatus="$(anypoint-cli runtime-mgr cloudhub-application describe "${appName}" -o json | jq .Status)"
+#echo "----->App status ->$appStatus<-"
+
+if [ "${appStatus}" == '"STARTED"' ]; 
+	then
+	echo "Condition status ----->true"
+	else
+	echo "Condition status ----->False"    
+fi 
+
 }
 
-function getAppsList(){
-    local offset=0
-    local limit=100
-    local apps_in_page=1
-    local appsJson=""
-
-    if [[ -z $APPS_LIST ]]; then 
-        APPS_LIST="[]"
-    fi 
-
-    while [[ ${apps_in_page} -gt 0 ]]; do 
-      	 testApp
-	 appsJson="$(anypoint-cli runtime-mgr cloudhub-application list -o json | jq .)"
-        apps_in_page=$(echo "$appsJson" | jq . | jq length)
-        if [[ ${apps_in_page} -gt 0 ]]; then 
-            APPS_LIST=$(echo "$APPS_LIST" | jq ". += $appsJson")
-        fi
-        ((offset=$offset+$limit))
-    done
-}
-
-deployApp
-getAppsList
-echo $1
+checkStatus $1
