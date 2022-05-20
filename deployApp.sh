b8a263252fef592117619b9dc76a67f8d2afa7a8
Ravi Dhyani <ravi.dhyani@ravidhy-ltm3zpb.internal.salesforce.com>
2022-05-20 09:51:29 +0100

    updated code

--- deployApp.sh
+++ deployApp.sh
@@ -5,7 +5,15 @@ checkStatusAndDeploy(){
 #source ./exports.sh
 echo "$ANYPOINT_PROFILE"
 local appName=$1
+local workerSize=$2
+local runTime=$3
+local workers=$4
+
 echo "--App name->${appName}"
+echo "--workerSize-->${workerSize}"
+echo "--runTime-->${runTime}"
+echo "--workers-->${workers}"
+
 local lowApp=$( echo "$appName" | tr '[:upper:]' '[:lower:]')
 echo "--Low App name->${lowApp}"
 #anypoint-cli
@@ -28,5 +36,5 @@ fi
 
 }
 
-checkStatusAndDeploy $1
+checkStatusAndDeploy $1 $2 $3 $4
 
