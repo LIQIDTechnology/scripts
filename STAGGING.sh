01a558c2eb8b6814841f85e639e353fb533a76e4
Ravi Dhyani <ravi.dhyani@ravidhy-ltm3zpb.internal.salesforce.com>
2022-05-20 11:36:05 +0100

    updated code

--- STAGGING.sh
+++ STAGGING.sh
@@ -1,1 +1,13 @@
 ## Stagging server details
+
+export ANYPOINT_ENV=STAGING
+
+#Environment Variables required to switch off SSL Verification in Python
+export MULE_RUNTIMEVERSION=4.4.0
+export MULE_DEPLOYMENTTYPE=cloudhub
+
+#Cloudhub Deployment Variables
+export MULE_CLOUDHUBWORKERS=1
+export MULE_CLOUDHUBWORKERSIZE=0.1
+export MULE_CLOUDHUBREGION=eu-central-1
+#export MULE_CLOUDHUBPROPERTIES="--property anypoint.platform.client_id:XXXXXX --property anypoint.platform.client_secret:YYYYYY"
